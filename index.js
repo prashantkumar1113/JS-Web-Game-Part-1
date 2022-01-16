7d198797019ca3cffacef7fe65b532970184d5c1
prashantkumar1113 <prashantkumar1113@gmail.com>
2022-01-16 10:24:30 +0530

    removed the position argument

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-function newImage(src, position, left, bottom) {
+function newImage(src, left, bottom) {
     let element = document.createElement("img");
     element.src = src;
-    element.style.position = position;
+    element.style.position = "fixed";
     element.style.left = left;
     element.style.bottom = bottom;
     document.body.append(element);
@@ -20,22 +20,22 @@ function newItem(src, position, left, bottom) {
 //Background
 for (let i = 0; i < window.innerWidth; i = i + 100) {
     let left = i + "px";
-    newImage("assets/grass.png", "fixed", left, "0");
-    newImage("assets/grass.png", "fixed", left, "100px");
-    newImage("assets/grass.png", "fixed", left, "200px");
-    newImage("assets/grass.png", "fixed", left, "300px");
-    newImage("assets/grass.png", "fixed", left, "400px");
+    newImage("assets/grass.png", left, "0");
+    newImage("assets/grass.png", left, "100px");
+    newImage("assets/grass.png", left, "200px");
+    newImage("assets/grass.png", left, "300px");
+    newImage("assets/grass.png", left, "400px");
 }
 
 //New Images
-newImage("assets/green-character.gif", "fixed", "100px", "100px");
-newImage("assets/pine-tree.png", "fixed", "450px", "200px");
-newImage("assets/tree.png", "fixed", "200px", "300px");
-newImage("assets/pillar.png", "fixed", "350px", "100px");
-newImage("assets/crate.png", "fixed", "150px", "200px");
-newImage("assets/well.png", "fixed", "500px", "425px");
+newImage("assets/green-character.gif", "100px", "100px");
+newImage("assets/pine-tree.png", "450px", "200px");
+newImage("assets/tree.png", "200px", "300px");
+newImage("assets/pillar.png", "350px", "100px");
+newImage("assets/crate.png", "150px", "200px");
+newImage("assets/well.png", "500px", "425px");
 
 //New Items
-newItem("assets/sword.png", "fixed", "500px", "405px");
-newItem("assets/sheild.png", "fixed", "165px", "185px");
-newItem("assets/staff.png", "fixed", "600px", "100px");
+newItem("assets/sword.png", "500px", "405px");
+newItem("assets/sheild.png", "165px", "185px");
+newItem("assets/staff.png", "600px", "100px");
